29922c84fad71de1879bd4e5f671e23311002e57
muraripes <murari.pes@gmail.com>
2020-01-16 02:04:20 +0530

    Login module added

--- react-with-jest-enzym/src/Components/login/Login.component.tsx
+++ react-with-jest-enzym/src/Components/login/Login.component.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import {ILogin} from "./interfaces";
+import { ILogin } from "./interfaces";
+import "./Login.component.css"
 
-const Login: React.FC<ILogin> = props => {
-    return <span>Login</span>
-}
+const Login: React.FC<ILogin> = () => {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email Address" />
+        <input type="submit" value="Subscribe" />
+      </form>
+    </>
+  );
+};
 
 export default Login;
